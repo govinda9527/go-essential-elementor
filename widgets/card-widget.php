43a3cce51ef024fe8349dfcdece80ef65a55f279
Govinda Dahal <govindahal05@gmail.com>
2023-10-10 17:53:39 +0545

    simple style functionality added

--- widgets/card-widget.php
+++ widgets/card-widget.php
@@ -7,7 +7,7 @@ class GOEE_Card_Widget extends \Elementor\Widget_Base
     }
     public function get_title()
     {
-        return esc_html__('Essenial Card', 'essential-elementor-widget');
+        return esc_html__('Essenial Card', 'go-ee');
     }
     public function get_icon()
     {
@@ -30,29 +30,102 @@ class GOEE_Card_Widget extends \Elementor\Widget_Base
         $this->start_controls_section(
             'content_section',
             [
-                'label' => esc_html__('Content', 'essential-elementor-widget'),
+                'label' => esc_html__('Content', 'go-ee'),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
         $this->add_control(
             'card_title',
             [
-                'label' => esc_html__('Card title', 'essential-elementor-widget'),
+                'label' => esc_html__('Card title', 'go-ee'),
                 'type' => \Elementor\Controls_Manager::TEXT,
                 'label_block' => true,
-                'placeholder' => esc_html__('Your card title here', 'essential-elementor-widget'),
+                'placeholder' => esc_html__('Your card title here', 'go-ee'),
             ]
         );
         $this->add_control(
             'card_description',
             [
-                'label' => esc_html__('Card Description', 'essential-elementor-widget'),
+                'label' => esc_html__('Card Description', 'go-ee'),
                 'type' => \Elementor\Controls_Manager::TEXTAREA,
                 'label_block' => true,
-                'placeholder' => esc_html__('Your card description here', 'essential-elementor-widget'),
+                'placeholder' => esc_html__('Your card description here', 'go-ee'),
             ]
         );
         $this->end_controls_section();
+        
+        // Style section
+        $this->start_controls_section(
+            'section_style',
+            [
+                'label' => esc_html__( 'Style', 'go-ee' ),
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+            ]
+        );
+        
+        // Color option
+        $this->add_control(
+            'title_options',
+            [
+                'label' => esc_html__( 'Title Options', 'essential-elementor-widget' ),
+                'type' => \Elementor\Controls_Manager::HEADING,
+                'separator' => 'before',
+            ]
+        );
+        $this->add_control(
+            'title_color',
+            [
+                'label' => esc_html__( 'Color', 'essential-elementor-widget' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'default' => '#030303',
+                'selectors' => [
+                    '{{WRAPPER}} h3' => 'color: {{VALUE}}',
+                ],
+            ]
+        );
+        
+        // Typography option
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'title_typography',
+                'selector' => '{{WRAPPER}} h3',
+            ]
+        );
+
+        // Options widget Description
+
+        $this->add_control(
+            'description_options',
+            [
+                'label' => esc_html__( 'Description Options', 'essential-elementor-widget' ),
+                'type' => \Elementor\Controls_Manager::HEADING,
+                'separator' => 'before',
+            ]
+        );      
+    
+        $this->add_control(
+            'description_color',
+            [
+                'label' => esc_html__( 'Color', 'essential-elementor-widget' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'default' => '#fff',
+                'selectors' => [
+                    '{{WRAPPER}} .card__description' => 'color: {{VALUE}}',
+                ],
+            ]
+        );
+    
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'description_typography',
+                'selector' => '{{WRAPPER}} .card__description',
+            ]
+        );
+
+        
+        $this->end_controls_section();	
     }
     protected function render()
     {
