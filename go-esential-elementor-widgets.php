b60cbaca224663fcad8186ed9a5fb35bf18f0dc6
Govinda Dahal <govindahal05@gmail.com>
2023-10-10 17:20:55 +0545

    initial commit

---
+++ go-esential-elementor-widgets.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Go Essential Elementor Widgets
+ * Description: Elementor custom widgets from Eessential Web Apps.
+ * Plugin URI:  https://govindahal.com.np/
+ * Version:     1.0.0
+ * Author:      Govinda Dahal
+ * Author URI:  https://govindahal.com.np/
+ * Text Domain: go-e2-widget
+ *
+ * Elementor tested up to: 3.5.0
+ * Elementor Pro tested up to: 3.5.0
+ */
